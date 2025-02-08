3ff069646ffcf66c7b360129bd32bd80dd1b1b1b
Adrian Gri <adrian.gri03@gmail.com>
2025-02-07 22:42:12 -0500

    Run workflow

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, BackgroundTasks
 import os
-from status import get_workflow_status
+from workflow import get_workflow_status, run_workflow
+import uuid
 
 app = FastAPI()
 
@@ -8,8 +9,15 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/run-workflow")
+async def post_run_workflow(body: dict, background_tasks: BackgroundTasks):
+    workflow_id = str(uuid.uuid4())
+    background_tasks.add_task(run_workflow, workflow_id, body["workflow"])
+
+    return { "workflow_id": workflow_id }
+
 @app.get("/workflow-status/{workflow_id}")
-async def read_item(workflow_id: str):
+async def workflow_status(workflow_id: str):
     return get_workflow_status(workflow_id)
 
 @app.post("/upload-file")
