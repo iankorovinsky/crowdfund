f2d05c5b7d9dd2eef8cb0a91f7ac0e67084f9f43
Adrian Gri <adrian.gri03@gmail.com>
2025-02-07 21:19:37 -0500

    Upload file endpoint

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
 from status import get_workflow_status
 
 app = FastAPI()
@@ -10,3 +11,11 @@ async def root():
 @app.get("/workflow-status/{workflow_id}")
 async def read_item(workflow_id: str):
     return get_workflow_status(workflow_id)
+
+@app.post("/upload-file")
+async def upload_python_file(file: UploadFile = File(...)):
+    file_location = f"/tmp/crowdfund/{file.filename}"
+    os.makedirs(os.path.dirname(file_location), exist_ok=True)
+    with open(file_location, "wb") as f:
+        f.write(await file.read())
+    return {"info": f"file '{file.filename}' saved at '{file_location}'"}
