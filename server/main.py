d5806e58d1111c9fd34e545b37ddbf87effe9bbe
Adrian Gri <adrian.gri03@gmail.com>
2025-02-07 21:05:23 -0500

    Workflow status endpoint

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from status import get_workflow_status
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/workflow-status/{workflow_id}")
+async def read_item(workflow_id: str):
+    return get_workflow_status(workflow_id)
